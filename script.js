8df498a5d654704abfcb8bcbcf7c07cc86d93bfb
kemillymarcondescardoso-sudo <kemilly.marcondes.cardoso@escola.pr.gov.br>
2026-06-01 20:17:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.getElementById("formulario").addEventListener("submit", function(e){
+    e.preventDefault();
 
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("resposta").innerHTML =
+    `🌸 Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`;
+
+    this.reset();
+});
